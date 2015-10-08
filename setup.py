49fd132310fb93c0cf1da23c1e4a3415fa57bed2
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-08 11:19:12 -0300

    Starting a new flask app.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='flask-wiki',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/gcavalcante8808/flask-wiki',
+    license='BSD',
+    author='gcavalcante8808',
+    author_email='gabriel.cavalcante88@gmail.cmo',
+    description='A Wiki written with Flask FrameWork'
+)
