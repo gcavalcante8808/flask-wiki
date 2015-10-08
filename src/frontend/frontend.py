d1d897af45e2c6aacbc97aad5f786557090a302c
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-08 12:49:01 -0300

    Basic template verification added.

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -8,6 +8,11 @@ frontend = Blueprint(__file__, __name__,
 @frontend.route('/', defaults={'page': 'index'})
 @frontend.route('/<page>')
 def show(page):
+    """
+    Try to Delive a page or abort with a 404 error.
+    :param page: name of the page
+    :return: template.
+    """
     try:
         return render_template('pages/%s.html' % page)
     except (TemplateNotFound,):
