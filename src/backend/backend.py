8486427f9b6fe3dbd8220690caca82bb29aafb8a
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-08 16:40:19 -0300

    Added Flask-Script and Flask-Migrate initial stub for the project.

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -1,22 +1,37 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.script import Manager
 from flask_restful import Api
 from src.backend.views import PageView
 
+
 #TODO: Verify URL prefix implementation if debug is true.
+
+PROJECT_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 app.config['TESTING'] = True
 
 
 app.config['DATABASE'] = 'wiki.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////%s' % app.config.get('DATABASE')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////%s/%s' % (PROJECT_ROOT_PATH,
+                                                             app.config.get('DATABASE'))
+
 api = Api(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app)
+manager = Manager(app)
 
+manager.add_command('db', MigrateCommand)
 
-def init_db(db):
-    # Create the db structure.
+if app.config['TESTING']:
+    from src.backend.models import *
     db.create_all()
+    migrate.init_app(app, db)
 
 api.add_resource(PageView,'/pages-list', endpoint='pages_list')
+
+if __name__ == '__main__':
+    manager.run()
