0207aafc2ffcf3ec1a283059141255034aa9b203
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-08 17:40:53 -0300

    MarshMallow support added. Circular imports on views avoided with ugly solution.

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -1,36 +1,43 @@
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 from flask_restful import Api
-from src.backend.views import PageView
+from flask_marshmallow import Marshmallow
 
 
-#TODO: Verify URL prefix implementation if debug is true.
+# TODO: Verify URL prefix implementation if debug is true.
+# TODO: Verify Migrate Support.
 
 PROJECT_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 app.config['TESTING'] = True
+app.config['BOOTSTRAP'] = False
 
 
 app.config['DATABASE'] = 'wiki.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////%s/%s' % (PROJECT_ROOT_PATH,
-                                                             app.config.get('DATABASE'))
+                                                              app.config.get('DATABASE'))
 
 api = Api(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app)
 manager = Manager(app)
+marsh = Marshmallow(app)
 
-manager.add_command('db', MigrateCommand)
 
-if app.config['TESTING']:
+# Database Configuration
+if app.config['BOOTSTRAP'] is True:
     from src.backend.models import *
+    db = SQLAlchemy(app, session_options={
+        'expire_on_commit': False
+    })
     db.create_all()
-    migrate.init_app(app, db)
+else:
+    db = SQLAlchemy(app)
 
+# Routing Configuration.
+# TODO: Avoiding Circular imports in this way is UGLY. Verify.
+from src.backend.views import PageView
 api.add_resource(PageView,'/pages-list', endpoint='pages_list')
 
 if __name__ == '__main__':
