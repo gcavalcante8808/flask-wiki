a664c23c6d919e91809d77cb5b7e2614e56f95a4
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-08 17:43:10 -0300

    A simples serializer for Page was created.

---
+++ src/backend/serializers.py
@@ -0,0 +1,13 @@
+from src.backend.backend import marsh
+from marshmallow import fields
+
+
+class PageSchema(marsh.Schema):
+    name = fields.String()
+    path = fields.String()
+    raw_content = fields.String()
+    rendered_content = fields.String()
+
+
+page_schema = PageSchema()
+pages_schema = PageSchema(many=True)
