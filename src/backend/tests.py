556a6a71925443358cacf27674009e261eb89809
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-08 17:43:49 -0300

    Page Mockup and pages-list verification is now supported.

--- src/backend/tests.py
+++ src/backend/tests.py
@@ -1,15 +1,25 @@
 import unittest
 import os
 import tempfile
-from src.backend.backend import app
+from src.backend.backend import app, db
+
 
 #TODO: Implement url_for based http ops.
+#TODO: Try to use mixer to mock the tests.
 class BackendTestCase(unittest.TestCase):
     def setUp(self):
         # For Now, creates a temporary Database.
         self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
         app.config['TESTING'] = True
         self.app = app.test_client()
+
+        from src.backend.models import Page
+        self.page = Page()
+        self.page.name = 'MainPage'
+        self.page.path = '/'
+
+        db.session.add(self.page)
+        db.session.commit()
 
     def tearDown(self):
         # For now, unlink the temp db created.
@@ -19,6 +29,7 @@ class BackendTestCase(unittest.TestCase):
     def test_get_list_of_pages(self):
         request = self.app.get('/pages-list')
         self.assertEqual(request.status_code, 200)
+        self.assertIn(self.page.name, str(request.data))
 
 if __name__ == '__main__':
     unittest.main()
