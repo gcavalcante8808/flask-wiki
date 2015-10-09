78e698712ffc9bad3afd0760b2f61158e449e4e2
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-09 12:11:48 -0300

    Starting to use the New GUID field.

---
+++ flask_wiki/backend/custom_serialization_fields.py
@@ -0,0 +1,14 @@
+from marshmallow import fields
+import uuid
+
+
+class GUIDSerializationField(fields.Field):
+    def _serialize(self, value, attr, obj):
+        print(value)
+        if value is None:
+            return value
+        else:
+            if isinstance(value, uuid.UUID):
+                return str(value)
+            else:
+                return None
