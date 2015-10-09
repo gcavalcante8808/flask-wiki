9a39e8f7903a72d00360628f52be7065ae12165b
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-09 12:11:34 -0300

    Starting to use the New GUID field.

--- flask_wiki/backend/serializers.py
+++ flask_wiki/backend/serializers.py
@@ -1,8 +1,10 @@
 from flask_wiki.backend.backend import marsh
+from flask_wiki.backend.custom_serialization_fields import GUIDSerializationField
 from marshmallow import fields
 
 
 class PageSchema(marsh.Schema):
+    guid = GUIDSerializationField()
     name = fields.String()
     path = fields.String()
     raw_content = fields.String()
