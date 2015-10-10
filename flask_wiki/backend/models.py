165d3023879bb544e6972df0516dd9107461d81a
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-10 20:24:03 -0300

    Added constraints and default for guid and rgt.
    Guids now receives uuid.uuid4() per default value.
    RGT now receives LFT+1 as default value.
    
    Constraints added for supporte Nested Sets Hierarchy added. The job is not complete yet.

--- flask_wiki/backend/models.py
+++ flask_wiki/backend/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import UniqueConstraint
+import uuid
+from sqlalchemy import UniqueConstraint, CheckConstraint, Index
+from sqlalchemy.sql.expression import text
+from sqlalchemy import event
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_wiki.backend.custom_fields import GUIDField
 
@@ -13,17 +16,55 @@ class Page(db.Model):
     left and right methods (named lft and rgt because of SQL99
     name restrictions)
     """
-    guid = db.Column(GUIDField, primary_key=True)
-    name = db.Column(db.String)
-    lft = db.Column(db.Integer)
+    guid = db.Column(GUIDField, primary_key=True, default=uuid.uuid4())
+    name = db.Column(db.String, nullable=False)
+    lft = db.Column(db.Integer, nullable=False)
     rgt = db.Column(db.Integer)
     raw_content = db.Column(db.Text)
     rendered_content = db.Column(db.Text)
 
+    # UniqueTogether lft and rgt
     UniqueConstraint('lft', 'rgt')
 
+    # Lft value should be inferior to rgt value.
+    CheckConstraint('lft < rgt', name='valid_range_pair')
+
+    # The RGT value should be gt 1
+    CheckConstraint('rgt > 1', name='valid_rgt')
+
+    # Minimal value for lft is 1 (root Node)
+    CheckConstraint('lft > 0', name='valid_lft')
+
+    # Weight should not be negative
+    CheckConstraint('wgt >= 0', name='non_negative_wgt')
+
+    # Quantity should not be negative
+    CheckConstraint('qty > 0', name='positive_qty')
+
+    # Index for lft and rgt
+    Index('idx_lft_rgt', 'lft', 'rgt', unique=True)
+
     def __repr__(self):
-        return "<Page %i,%i %s>" % (self.lft, self.rgt, self.name)
+        return "<Page %s,%s %s>" % (self.lft, self.rgt, self.name)
 
     def __str__(self):
         return self.name
+
+    @property
+    def root(self):
+        # Return the root of the tree
+        result = self.query.filter_by(lft=1).all()
+        return result
+
+    @property
+    def leafs(self):
+        # Return list of Leafs (Nodes without Children)
+        result = self.query.filter(text('lft = (rgt - 1)')).all()
+        return result
+
+
+@event.listens_for(Page, 'before_insert')
+def page_defaults(mapper, configuration, target):
+    # If no RGT is provided, one will be generated using lft column.
+    if not target.rgt:
+        target.rgt = target.lft + 1
