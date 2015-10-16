1fd78aba0846740a5591afa07e1415b90b99a6ce
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-15 23:40:42 -0300

    Docs updated, added stubs for put and patch on PageView.

--- flask_wiki/backend/views.py
+++ flask_wiki/backend/views.py
@@ -5,11 +5,13 @@ from flask_wiki.backend.serializers import pages_schema, page_schema
 
 class PageView(Resource):
     def get(self):
+        # Just Dump all Pages for now.
         pages = Page.query.all()
         result = pages_schema.dump(pages)
         return result.data
 
     def post(self):
+        # Get a a object and verify if exists; if it does, return a 422 code and if not, create it.
         parser = reqparse.RequestParser()
         parser.add_argument('raw_content', type=str)
         parser.add_argument('name', type=str)
@@ -21,7 +23,7 @@ class PageView(Resource):
             r = Page.query.filter_by(name=serializer.data.get('name', None)).first()
             if r:
                 return {'data': serializer.data,
-                        'message': "The object already exist. Use PATC to update the object."}, 422
+                        'message': "The object already exist. Use PATCH to update the object."}, 422
 
             result = Page()
             result.raw_content = serializer.data.get('raw_content')
@@ -32,3 +34,11 @@ class PageView(Resource):
             return {'data': serializer.data}, 201
 
         return serializer.errors, 400
+
+    def put(self):
+        # Stub for NotImplemented
+        return 501
+
+    def patch(self):
+        # Stub for NotImplemented
+        return 501
