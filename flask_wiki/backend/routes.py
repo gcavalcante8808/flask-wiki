6086b970e6c37ca4f343291a35bbb9e533109c1c
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-16 00:30:41 -0300

    Support for page-detail url added.

--- flask_wiki/backend/routes.py
+++ flask_wiki/backend/routes.py
@@ -1,4 +1,5 @@
 from flask_wiki.backend.backend import api
-from flask_wiki.backend.views import PageView
+from flask_wiki.backend.views import PageView, PageDetail
 
 api.add_resource(PageView, '/pages-list', endpoint='pages-list')
+api.add_resource(PageDetail, '/pages/<slug>', endpoint='page-detail')
