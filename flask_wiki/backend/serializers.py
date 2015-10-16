27d4c46974788922a6e19b5db87585d3f9d7c155
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-15 21:43:02 -0300

    For now, ModelSchema is throwing errors about the GUIDFIeld and Schema will be used.

--- flask_wiki/backend/serializers.py
+++ flask_wiki/backend/serializers.py
@@ -1,14 +1,18 @@
 from flask_wiki.backend.backend import marsh
 from flask_wiki.backend.custom_serialization_fields import GUIDSerializationField
+from flask_wiki.backend.models import Page
 from marshmallow import fields
 
-
+# TODO: Change it for ModelSchema.
 class PageSchema(marsh.Schema):
+    class Meta:
+        model = Page
+
     guid = GUIDSerializationField()
     name = fields.String()
-    path = fields.String()
     raw_content = fields.String()
     rendered_content = fields.String()
+
 
 
 page_schema = PageSchema()
