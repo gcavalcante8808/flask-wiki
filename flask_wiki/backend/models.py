964429be38ac1a91e1d3669f42782e5dcfc868be
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2015-10-16 00:31:04 -0300

    Slug and slugify support added for page.

--- flask_wiki/backend/models.py
+++ flask_wiki/backend/models.py
@@ -1,10 +1,10 @@
 import uuid
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import UniqueConstraint
+from sqlalchemy import UniqueConstraint, event
 from flask_wiki.backend.custom_fields import GUIDField
+from slugify import slugify
 
 db = SQLAlchemy()
-
 
 #TODO: Add Owner and other security fields later.
 class Page(db.Model):
@@ -15,6 +15,7 @@ class Page(db.Model):
     name = db.Column(db.String, nullable=False, unique=True)
     raw_content = db.Column(db.Text, nullable=False, unique=True)
     rendered_content = db.Column(db.Text)
+    slug = db.Column(db.String, nullable=False, unique=True)
 
     UniqueConstraint('name', 'raw_content')
 
@@ -23,3 +24,9 @@ class Page(db.Model):
 
     def __str__(self):
         return self.name
+
+
+def make_slug(mapper, connection, target):
+    target.slug = slugify(target.name)
+
+event.listen(Page, 'before_insert', make_slug)
